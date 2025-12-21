7a036b3d17c2ace54b1be8546547983f2bacd004
Jorge Calheiros <86486202+jorgecalheiros@users.noreply.github.com>
2025-12-21 14:31:28 -0300

    ADD: Adicionando ServiceCollectionExtension para disponiblizar a configuração completa da camada de infra.

---
+++ SalesManager.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesManager.Contracts.Interfaces.Repositories;
+using SalesManager.Domain.Interfaces.Repositories;
+using SalesManager.Infrastructure.Data.PostgreSql;
+using SalesManager.Infrastructure.Data.PostgreSql.Repositories;
+
+namespace SalesManager.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, string connectinString)
+        {
+            services.AddDbContext<SalesManagerDbContext>(options => options.UseNpgsql(connectinString));
+
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IClientReadRepository, ClientReadRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<ISaleRepository, SaleRepository>();
+            services.AddScoped<ISaleReadRepository, SaleReadRepository>();
+
+            return services;
+        }
+    }
+}

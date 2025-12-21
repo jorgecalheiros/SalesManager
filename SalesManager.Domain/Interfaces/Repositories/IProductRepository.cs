eb7a2c15b893c892232fc50f92220c1c7798b108
Jorge Calheiros <86486202+jorgecalheiros@users.noreply.github.com>
2025-12-21 19:10:59 -0300

    CHANGE: Removendo usings.

--- SalesManager.Domain/Interfaces/Repositories/IProductRepository.cs
+++ SalesManager.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace SalesManager.Domain.Interfaces.Repositories
+﻿namespace SalesManager.Domain.Interfaces.Repositories
 {
     public interface IProductRepository : IBaseRepository<Entities.Product>
     {
